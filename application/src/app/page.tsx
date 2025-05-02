5cf85bdde9464537d66499c5a8f2dcc7d7c464ce
driano-gael <97949425+driano-gael@users.noreply.github.com>
2025-05-02 09:50:03 +0200

    test deploi 1

---
+++ application/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <p>demarrage du projet front pour les admins</p>
+    <button className="btn btn-primary">test daisy</button>
+    </>
+  );
+}
