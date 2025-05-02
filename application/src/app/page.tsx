52a7c60e929144d8fdf4c4d2331148d32a50f81c
driano-gael <97949425+driano-gael@users.noreply.github.com>
2025-05-02 10:39:54 +0200

    confirmation deploiement

--- application/src/app/page.tsx
+++ application/src/app/page.tsx
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <>
     <p>demarrage du projet front pour les admins</p>
-    <button className="btn btn-primary">test daisy</button>
     </>
   );
 }
